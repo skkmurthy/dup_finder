8f685c332bcf960b9dd9ffce198780d837562baf
Sathya Krishna Murthy <sathya.krishnamurthy@emc.com>
2016-12-23 01:30:48 -0500

    inital version of Comparator

---
+++ Comparator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import Directory
+
+class Comparator:
+    def __init__(self, refDir, candidateDir):
+        self.refDir = Directory(refDir)
+        self.candidateDir = Directory(candidateDir)
+
+    def compare():
